a1f060510ef8e233a8ae7c0a46f8c6124de1d220
Dhanarooban1 <dhanazx1@gmail.com>
2025-03-05 12:08:31 +0530

    Error-message-added

--- Backend/middleware/responseHandler.js
+++ Backend/middleware/responseHandler.js
@@ -14,7 +14,7 @@ export const successHandler = (res, data, message = 'Success', statusCode = 200)
     const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode).json({
       status: 'error',
-      message: process.env.NODE_ENV === 'production' ? err  : err.message,
+      message: err?.message,
       stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     });
   };
